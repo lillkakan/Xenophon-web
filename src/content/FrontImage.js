977c5e4dd0fa2571edfbe8621fb98446b06ee9b7
Jesper Fasth <lillkakan@live.se>
2022-01-31 08:55:38 +0100

    Added description.

--- src/content/FrontImage.js
+++ src/content/FrontImage.js
@@ -5,7 +5,11 @@ import { useState } from 'react';
 
 function FrontImage (props) { 
 
+  const nameText = {name: "Xenophon", div: 8}
+  const descText = {name: "Athenian military leader, philosopher, and historian.", div: 30}
+
   const [imageSrc, setImageSrc] = useState(bigImage);
+  const [text, setText] = useState(nameText);
 
   function setImage () {
 
@@ -16,11 +20,20 @@ function FrontImage (props) {
     } 
   }
 
+  function toggleText () {
+
+    if (text.name === nameText.name){
+      setText(descText)
+    } else if (text.name === descText.name) {
+      setText(nameText)
+    } 
+  }
+
   return (
     <div className='container' >
       <img src={imageSrc} onClick={setImage} alt="bigImage" style={{width: "100%"}}/>
-      <div className='text-on-top-image'>
-        <h1 className='text' style={{fontSize: props.width/8}}>Xenophon</h1>
+      <div className='text-on-top-image' onClick={toggleText}>
+        <h1 className='text' style={{fontSize: props.width/text.div}} >{text.name}</h1>
       </div>
       <div className='text-on-bottom-image'>
         <h1 className='slogan' style={{fontSize: props.width/25}}>An ergonomic split Keyboard</h1>
